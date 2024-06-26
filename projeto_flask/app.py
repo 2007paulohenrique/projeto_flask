2339bc0246254c8f3933bb9c1b78b6cc70b04537
= <=>
2024-06-26 17:45:36 -0300

    leitura e escrita em arquivos com python

--- projeto_flask/app.py
+++ projeto_flask/app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-lista_produtos = [
-    {"nome": "Coca-cola", "descricao": "veneno", "preco": "10.00", "url_image": "https://www.shutterstock.com/image-vector/novi-sad-serbia-january-21-600nw-1008479416.jpg"},
-    {"nome": "Doritos", "descricao": "suja a mão", "preco": "8.00", "url_image": "https://m.media-amazon.com/images/I/610trEtCQuS._AC_UF1000,1000_QL80_.jpg"},
-    {"nome": "Agua", "descricao": "mata sede", "preco": "5.00", "url_image": "https://www.imigrantesbebidas.com.br/bebida/images/products/full/2893-agua-mineral-crystal-sem-gas-500ml.jpg"},
-]
+def listar_produtos():
+    with open("produtos.csv", "r") as file:
+        lista_produtos = []
+
+        for linha in file:
+            nome, descricao, preco, imagem = linha.strip().split(",")
+
+            produto = {
+                "nome": nome,
+                "descricao": descricao,
+                "preco": preco,
+                "imagem": imagem
+            }
+
+            lista_produtos.append(produto)
+
+        return lista_produtos
+    
+def add_produto(p):
+    with open("produtos.csv", "a") as file:
+        file.write(f"\n{p['nome']},{p['descricao']},{p['preco']},{p['imagem']}")
+
 
 app = Flask(__name__)
 
@@ -18,11 +35,11 @@ def contato():
 
 @app.route("/produtos")
 def produtos():
-    return render_template("produtos.html", produtos=lista_produtos)
+    return render_template("produtos.html", produtos=listar_produtos())
 
 @app.route("/produtos/<nome>")
 def produto(nome):
-    for produto in lista_produtos:
+    for produto in listar_produtos():
         if produto['nome'] == nome:
             return render_template("produto.html", produto=produto)
     return "Produto não existe!"
@@ -37,9 +54,9 @@ def salvar_produto():
     descricao = request.form["descricao"]
     preco = request.form["preco"]
     imagem = request.form["imagem"]
-    produto = {"nome":nome, "descricao":descricao, "preco":preco, "url_image": imagem}
+    produto = {"nome":nome, "descricao":descricao, "preco":preco, "imagem": imagem}
 
-    lista_produtos.append(produto)
+    add_produto(produto)
 
     return redirect(url_for("produtos"))
 
